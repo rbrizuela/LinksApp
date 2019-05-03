90fa45e947cec8d8f1d65064f1e38659b3dde2eb
Roberto Brizuela <robertojosebrizuela@hotmail.com>
2019-05-02 22:34:05 -0300

    commit

--- favorite-links/src/lib/passport.js
+++ favorite-links/src/lib/passport.js
@@ -3,6 +3,37 @@ const LocalStrategy = require('passport-local').Strategy
 const pool = require('../database')
 const helpers = require('../lib/helpers')
 
+
+passport.use('local.signin', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, username, password, done) => {
+
+  console.log(req.body)
+
+  const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username])
+  
+  if (rows.length > 0) {
+    
+    const user = rows[0]
+    const validPassword = await helpers.mathPassword(password, user.password)
+
+    if (validPassword) {
+      done(null, user, req.flash('success', 'Welcome' + user.username))
+    } else {
+      done(null, false, req.flash('message','Password incorrect'))
+    }
+
+  } else {
+
+    return done(null, false, req.flash('message','Username not exist'))
+
+  }
+
+}))
+
+
 passport.use('local.signup', new LocalStrategy({
   usernameField: 'username',
   passwordField: 'password',
